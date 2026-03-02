755d93a299c17bd37cde9cab6585100c24667186
AdaleeVorbeck <adavor0529@students.maquoketaschools.org>
2026-03-02 19:59:12 +0000

    LET YOU BACK INTO MY LIFE

--- Calculator.cs
+++ Calculator.cs
@@ -8,5 +8,14 @@ public class Calculator
 
     public void Calculate(){
         // Add your code here
+        Addition = Addition + 5;
+        Subtraction = Subtraction - 1;
+        Multiplication = Multiplication * 7;
+        Division = Division / 2;
     }
 }
+
+//should use compound assignment to add 5 to the Addition
+//should use compound assignment to subtract 1 from the Subtraction
+//should use compound assignment to multiply the Multiplication by 7
+//should use compound assignment to divide the Division by 2
